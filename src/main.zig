8ba7cf10c83264b62fd1f4c6a8dfecf9d5791a0a
Marnix Klooster <marnix.klooster@gmail.com>
2022-12-30 22:08:31 +0100

    Fix timer, PSC is buffered.

--- src/main.zig
+++ src/main.zig
@@ -16,28 +16,36 @@ pub const TIM6Timer = struct {
             .OPM = 1,
         });
 
-        // Set prescaler to roughly 1ms per count.
-        // Here we assume TIM6 is running on an 8 MHz clock,
-        // which it is by default after STM32F3DISCOVERY MCU reset.
-        regs.TIM6.PSC.raw = 7999;
-
         return @This(){};
     }
 
     pub fn delayMs(_: @This(), n: u16) void {
         if (n == 0) return; // to avoid counting to 2**16
 
+        // Set prescaler to roughly 1ms per count.
+        // Here we assume TIM6 is running on an 8 MHz clock,
+        // which it is by default after STM32F3DISCOVERY MCU reset.
+        regs.TIM6.PSC.raw = 7999;
+        regs.TIM6.EGR.modify(.{ .UG = 1 }); // so that PSC is picked up and CNT reset, sets UIF
+        regs.TIM6.SR.modify(.{ .UIF = 0 }); // clear UIF
+
         // Set our value for TIM6 to count to.
-        regs.TIM6.ARR.raw = n;
+        regs.TIM6.ARR.raw = n; // picked up directly as ARPE = 0 by default
 
         // Start the clock using CEN.
         regs.TIM6.CR1.modify(.{ .CEN = 1 });
 
         // Wait for TIM6 to set the status register.
         while (regs.TIM6.SR.read().UIF == 0) {}
+    }
 
-        // Clear the status register.
-        regs.TIM6.SR.modify(.{ .UIF = 0 });
+    pub fn click(_: @This()) void {
+        regs.TIM6.PSC.raw = 1; // no scaling down, use clock frequency
+        regs.TIM6.EGR.modify(.{ .UG = 1 }); // so that PSC is picked up and CNT reset, sets UIF
+        regs.TIM6.SR.modify(.{ .UIF = 0 }); // clear UIF
+        regs.TIM6.ARR.raw = 1; // picked up directly as ARPE = 0 by default
+        regs.TIM6.CR1.modify(.{ .CEN = 1 });
+        while (regs.TIM6.SR.read().UIF == 0) {}
     }
 };
 
